f3addca8df90a8c9a1fd29dc7f02c671b9585765
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-06-08 11:37:43 +0200

    Extends API usage in App

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -5,14 +5,17 @@ using Pets;
 namespace ConsoleApplication {
     public class MyLittleZoo {
         public static void Main (string[] args) {
-            List<Pet> pets = new List<Pet> {
-                new Dog ("Willy", Rooms.DiningRoom),
-                new Cat ("KatziTatzi", Rooms.Kitchen)
-            };
+            Dog Willy = new Dog("Willy", Rooms.DiningRoom);
+            Cat Kitty = new Cat ("KatziTatzi", Rooms.Kitchen);
+            List<Pet> pets = new List<Pet> {Willy, Kitty};
 
             foreach (var pet in pets) {
                 Console.WriteLine (pet.TalkToOwner ());
             }
+
+            Willy.SearchForCat(Kitty);
+            Willy.LocalizedInRoom = Rooms.Kitchen;
+            Willy.SearchForCat(Kitty);
         }
     }
 }
