5a844eb165fa6babb5213bb78d6b529b1c55a02c
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-06-09 23:51:05 +0200

    Add test cases

---
+++ src/Tests/PetTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Pets;
+using Xunit;
+
+namespace Pets.Tests
+{
+    public class PetTests {
+        [Fact]
+        public void DogTalkToOwnerReturnsWoof () {
+            // Arrange
+            string expected = "Woof!";
+            Dog Willy = new Dog("Willy", Rooms.DiningRoom);
+            // Act
+            string actual = Willy.TalkToOwner ();
+            // Assert
+            Assert.Equal (expected, actual);
+        }
+
+        [Fact]
+        public void CatTalkToOwnerReturnsMeow () {
+            string pattern = "Meow!";
+            Cat Kitty = new Cat ("KatziTatzi", Rooms.Kitchen);
+            string actual = Kitty.TalkToOwner ();
+
+            Assert.Contains (pattern, actual);
+        }
+
+    }
+}
